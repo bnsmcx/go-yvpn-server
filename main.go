0bf4c17c035a75c1f24a89123f3b702caa35fef0
ben <ben@bnsmcx.com>
2023-07-30 14:18:03 +0700

    Remove all swagger stuff

--- main.go
+++ main.go
@@ -6,40 +6,20 @@ import (
 	"github.com/digitalocean/godo"
 	"github.com/go-chi/chi/v5"
 	"github.com/google/uuid"
-	httpSwagger "github.com/swaggo/http-swagger"
 	"log"
 	"net/http"
 	"os"
-	"strings"
 	"yvpn_server/auth"
 	"yvpn_server/db"
-	_ "yvpn_server/docs"
 	"yvpn_server/ux"
 )
 
-// @title yvpn API
-// @version 0.0.1
-// @description The public API for the yvpn server.
-// @contact.name bnsmcx
-// @contact.url https://github.com/bnsmcx/go-yvpn-server
-// @securityDefinitions.apikey	ApiKeyAuth
-// @in							header
-// @name						Authorization
-// @license.name TODO
-// @license.url TODO
-// @host localhost:8000
-// @BasePath /
 func main() {
 	r := chi.NewRouter()
 	err := db.Connect()
 	if err != nil {
 		log.Fatalln(err)
 	}
-
-	// serve docs
-	r.Get("/docs/*", httpSwagger.Handler(
-		httpSwagger.URL("http://localhost:8000/docs/doc.json"), //The url pointing to API definition
-	))
 
 	// Public Routes
 	r.Group(func(r chi.Router) {
@@ -56,18 +36,6 @@ func main() {
 		r.Get("/manage", ux.RenderManage)
 	})
 
-	// Admin Routes
-	//r.Group(func(r chi.Router) {
-	//	r.Use(AuthAdmin)
-	//	r.Post("/admin", CreateAsset)
-	//})
-
-	// API routes
-	r.Group(func(r chi.Router) {
-		r.Get("/api/endpoint", CheckBearer(HandleGetEndpoints))
-		r.Post("/api/endpoint", CheckBearer(HandleCreateEndpoint))
-	})
-
 	log.Fatalln(http.ListenAndServe(":8000", r))
 }
 
@@ -75,17 +43,6 @@ type Datacenter struct {
 	Datacenter string `json:"datacenter"`
 }
 
-// HandleCreateEndpoint
-// @Summary		Create a new endpoint
-// @Description	create a new endpoint in the specified datacenter
-// @Tags		endpoint
-// @Security ApiKeyAuth
-// @Accept 		json
-// @Produce		text/plain
-// @param token body Datacenter true "Datacenter"
-// @Success		200 {string} string "new config"
-// @Failure		400
-// @Router		/api/endpoint [post]
 func HandleCreateEndpoint(w http.ResponseWriter, r *http.Request) {
 	var dc Datacenter
 	err := json.NewDecoder(r.Body).Decode(&dc)
@@ -133,44 +90,4 @@ func HandleCreateEndpoint(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-}
-
-// HandleGetEndpoints
-// @Summary		Get all endpoints
-// @Description	get all of a user's endpoints
-// @Tags			endpoint
-// @Security ApiKeyAuth
-// @Produce		json
-// @Success		200
-// @Failure		400
-// @Router			/api/endpoint [get]
-func HandleGetEndpoints(w http.ResponseWriter, r *http.Request) {
-	//TODO:  Get userID from Bearer token, return all their endpoints from db
-}
-
-// CheckBearer is middleware that validates the bearer token
-func CheckBearer(next http.HandlerFunc) http.HandlerFunc {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Get Authorization header
-		authHeader := r.Header.Get("Authorization")
-
-		// Split the header value by space
-		headerParts := strings.Split(authHeader, " ")
-
-		validFormat := len(headerParts) == 2 && strings.ToLower(headerParts[0]) == "bearer"
-		if !validFormat {
-			http.Error(w, "Invalid or missing Authorization header", http.StatusBadRequest)
-			return
-		}
-
-		account, err := db.GetAccountByBearer(headerParts[1])
-		if err != nil {
-			http.Error(w, "Invalid or missing Authorization header", http.StatusBadRequest)
-			return
-		}
-
-		// If all is good, call the next middleware or final handler
-		next.ServeHTTP(w, r.WithContext(
-			context.WithValue(r.Context(), "account", account.ID)))
-	})
 }
