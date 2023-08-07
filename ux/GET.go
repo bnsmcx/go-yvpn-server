2a966943b35b0e9403fc548df7639ffb0f770fbd
ben <ben@bnsmcx.com>
2023-08-07 17:00:40 +0700

    Clear out CSS, have 'sign up' button direct to /signup

--- ux/GET.go
+++ ux/GET.go
@@ -19,7 +19,17 @@ func RenderLanding(w http.ResponseWriter, r *http.Request) {
 	}
 }
 func RenderSignup(w http.ResponseWriter, r *http.Request) {
-	return
+	// Parse the templates
+	tmpl, err := template.ParseFiles("templates/base.html", "templates/signup.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Execute the "layout" template and send it to the ResponseWriter.
+	if err := tmpl.Execute(w, nil); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
 
 func RenderLogin(w http.ResponseWriter, r *http.Request) {
