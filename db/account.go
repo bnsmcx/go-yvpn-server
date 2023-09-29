37d6c02e7f34e2996e3b4f914c2283e687678825
ben <ben@bnsmcx.com>
2023-09-30 02:53:24 +0700

    Implement account activation

--- db/account.go
+++ db/account.go
@@ -3,16 +3,28 @@ package db
 import (
 	"fmt"
 	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
 )
 
 // Account defines the Account record
 type Account struct {
 	ID                uuid.UUID `gorm:"type:uuid;primaryKey;"`
+	Activated         bool
 	Email             string
 	Password          []byte
 	BearerToken       string
 	DigitalOceanToken string
 	Endpoints         []Endpoint `gorm:"foreignKey:AccountID"`
+}
+
+func (a *Account) Activate(password string) error {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	a.Password = hash
+	a.Activated = true
+	return a.Save()
 }
 
 func (a *Account) Save() error {
