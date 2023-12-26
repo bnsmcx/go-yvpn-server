a3a4e6ce3e8551e20a88d20c549e494727fe4417
ben <ben@bnsmcx.com>
2023-12-25 17:55:04 -0900

    Add helper function to redirect users to login if bad session

--- auth/middleware.go
+++ auth/middleware.go
@@ -11,20 +11,29 @@ func MandateSession(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		cookie, err := r.Cookie("session_id")
 		if err != nil {
-			log.Println("missing session_id")
-			http.Redirect(w, r, "/login", http.StatusSeeOther)
+			log.Println("invalid or missing session cookie: ", err)
+			redirectToLogin(w, r)
 			return
 		}
 		id, ok := getSession(cookie.Value)
 		if !ok {
-			log.Println("invalid session_id")
-			http.Redirect(w, r, "/login", http.StatusSeeOther)
+			log.Println("invalid session cookie: ", cookie.Value)
+			redirectToLogin(w, r)
 			return
 		}
 		ctx := context.WithValue(r.Context(), "id", id)
 		ctx = context.WithValue(ctx, "session_id", cookie.Value)
 		next.ServeHTTP(w, r.WithContext(ctx))
 	})
+}
+
+func redirectToLogin(w http.ResponseWriter, r *http.Request) {
+	if r.Header.Get("Hx-Request") == "true" {
+		w.Header().Set("HX-Redirect", "/login")
+		return
+	}
+	http.Redirect(w, r, "/login", http.StatusSeeOther)
+	return
 }
 
 // CheckSession checks to see if a user is logged in without forcing a redirect to /login
